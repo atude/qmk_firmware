3325e9da2aa07fc96169b88817002890f4d1abe9
Mozamel Anwary <mozamel.anwary1@gmail.com>
2022-07-11 13:54:59 +1000

    enable certain pets via atyu config

--- keyboards/cannonkeys/satisfaction75/atyu_config.h
+++ keyboards/cannonkeys/satisfaction75/atyu_config.h
@@ -5,8 +5,10 @@
 #define OLED_CLOCK_ENABLED false
 #define OLED_BONGO_ENABLED true
 #define OLED_PETS_ENABLED true
-
 #define OLED_BONGO_FILLED false
+#define OLED_PET_LUNA_ENABLED true
+#define OLED_PET_KIRBY_ENABLED false
+#define OLED_PET_PUSHEEN_ENABLED true
 
 enum oled_modes {
     OLED_DEFAULT,
@@ -25,9 +27,15 @@ enum oled_modes {
 
 #if OLED_PETS_ENABLED
 enum pet_modes {
+#if OLED_PET_LUNA_ENABLED
     PET_LUNA,
+#endif
+#if OLED_PET_KIRBY_ENABLED
     PET_KIRBY,
+#endif
+#if OLED_PET_PUSHEEN_ENABLED
     PET_PUSHEEN,
+#endif
     _NUM_PET_MODES,
 };
 #endif
--- keyboards/cannonkeys/satisfaction75/components/draw_pets.h
+++ keyboards/cannonkeys/satisfaction75/components/draw_pets.h
@@ -2,16 +2,23 @@
 #include "pets/draw_luna.h"
 #include "pets/draw_kirby.h"
 #include "pets/draw_pusheen.h"
+#include "atyu_config.h"
 #include <stdio.h>
 
 void draw_current_pet(void) {
     switch (pet_mode) {
         default:
+#if OLED_PET_LUNA_ENABLED
         case PET_LUNA:
             return draw_luna();
-        case PET_PUSHEEN:
-            return draw_pusheen();
+#endif
+#if OLED_PET_KIRBY_ENABLED
         case PET_KIRBY:
             return draw_kirby();
+#endif
+#if OLED_PET_PUSHEEN_ENABLED
+        case PET_PUSHEEN:
+            return draw_pusheen();
+#endif
     }
 }
